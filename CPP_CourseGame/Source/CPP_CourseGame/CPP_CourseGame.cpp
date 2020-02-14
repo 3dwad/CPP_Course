4caebf79a9a82292a20fd8f0e94cb597e7c26071
Vadim <3dwad@mail.ru>
2020-02-14 23:16:36 +0300

    Initialize

---
+++ CPP_CourseGame/Source/CPP_CourseGame/CPP_CourseGame.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "CPP_CourseGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_CourseGame, "CPP_CourseGame" );
---
+++ CPP_CourseGame/Source/CPP_CourseGame/CPP_CourseGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_CourseGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_COURSEGAME_API ACPP_CourseGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
